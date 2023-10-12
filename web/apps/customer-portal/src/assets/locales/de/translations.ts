665a3331b2eb96db4792078c3905b1de2f824ff2
TechnoElf <technoelf@undertheprinter.com>
2023-10-12 02:24:26 +0200

    customer: adjust assets for galeriefest

--- web/apps/customer-portal/src/assets/locales/de/translations.ts
+++ web/apps/customer-portal/src/assets/locales/de/translations.ts
@@ -8,17 +8,17 @@ export const translations: NestedPartialAsStrings<Translations> = {
   StuStaPay: "StuStaPay",
   logout: "Logout",
   login: "Login",
-  userTagUid: "Bändchen-Chip ID",
-  userTagPin: "Bändchen-Chip Pin",
+  userTagUid: "Karten-Chip ID",
+  userTagPin: "Karten-Chip Pin",
   loginFailed: "Login fehlgeschlagen: {{reason}}.",
   errorLoadingCustomer: "Fehler beim Laden der Kundendaten",
-  payoutInfo: "Um dein Restguthaben nach dem Festival zu erhalten, <1>trage bitte deine Bankdaten hier ein</1>. Die erste Auszahlung findet voraussichtlich 3 Wochen nach Festivalende statt.",
+  payoutInfo: "",
   about: "Impressum",
   contact: "Kontakt",
-  wristbandTagExample: "Beispiel eines Bändchen-Chips",
-  wristbandTagExampleTitle: "Bändchen-Chip Beispiel mit PIN und ID",
+  wristbandTagExample: "Beispiel eines Karten-Chips",
+  wristbandTagExampleTitle: "Karten-Chip Beispiel mit PIN und ID",
   wristbandTagExampleDescription:
-    "Die Bändchen-Chip ID und PIN findest Du auf der Rückseite deines Bändchen-Chips. Sie sollte aussehen wie im unteren Beispiel:",
+    "Die Karten-Chip ID und PIN findest Du auf der Rückseite deiner Karte. Sie sollte aussehen wie im unteren Beispiel:",
   termsAndConditionsHeader: "Die Datenschutzbestimmungen können <1>hier</1> eingesehen werden.",
   privacyPolicyHeader: "Unsere AGBs können <1>hier</1> eingesehen werden.",
   languages: {
@@ -26,7 +26,7 @@ export const translations: NestedPartialAsStrings<Translations> = {
     de: "Deutsch",
   },
   balance: "Guthaben",
-  tagUid: "Bändchen-Chip ID",
+  tagUid: "Karten-Chip ID",
   vouchers: "Getränkemarken",
   order: {
     loadingError: "Fehler beim laden der Bestellungen",
@@ -45,30 +45,29 @@ export const translations: NestedPartialAsStrings<Translations> = {
     },
   },
   payout: {
-    iban: "IBAN",
-    bankAccountHolder: "Kontoinhaber",
-    email: "E-Mail",
-    info: "Damit wir dein Restguthaben überweisen können, trage bitte deine Bankdaten hier ein. Du kannst auch einen Teil oder Dein gesamtes Guthaben an uns spenden, um unser ehrenamtliches Engagement zu unterstützen. Kulturleben in der Studentenstadt e.V. is ein von Studierenden betriebener gemeinnütziger Verein, welcher jährlich das StuStaCulum-Festival organisiert. Die erste Auszahlung findet voraussichtlich 3 Wochen nach Festivalende statt.",
-    ibanNotValid: "ungültige IBAN",
-    countryCodeNotSupported: "IBAN Ländercode wird nicht unterstützt",
-    mustAcceptPrivacyPolicy: "Sie müssen die Datenschutserklärung akzeptieren",
-    privacyPolicyCheck: "Ich habe die <1>Datenschutzerklärung</1> des StuStaCulum gelesen und akzeptiere sie.",
-    errorFetchingData: "Fehler beim laden der Daten.",
-    updatedBankData: "Bankdaten erfolgreich aktualisiert. Die erste Auszahlung erfolgt voraussichtlich 3 Wochen nach Festivalende.",
-    errorWhileUpdatingBankData: "Fehler beim aktualisieren der Bankdaten.",
-    donationMustBePositive: "Das Spende muss positiv sein",
-    donationExceedsBalance: "Die Spende darf nicht den Kontostand überschreiten",
-    donationTitle: "Spende",
-    payoutTitle: "Auszahlung",
-    donationAmount: "Spendebetrag ",
-    donationDescription:
-      "Hat dir das Festival gefallen? Wir würden uns über eine Spende freuen, um unsere ehrenamtliche Arbeit zu unterstützen und zukünftige StuStaCula noch besser zu machen.",
-    donateRemainingBalanceOf: "Spende verbleibende Summe von ",
-    submitPayoutData: "Bankdaten speichern",
+    iban: "",
+    bankAccountHolder: "",
+    email: "",
+    info: "",
+    ibanNotValid: "",
+    countryCodeNotSupported: "",
+    mustAcceptPrivacyPolicy: "",
+    privacyPolicyCheck: "",
+    errorFetchingData: "",
+    updatedBankData: "",
+    errorWhileUpdatingBankData: "",
+    donationMustBePositive: "",
+    donationExceedsBalance: "",
+    donationTitle: "",
+    payoutTitle: "",
+    donationAmount: "",
+    donationDescription: "",
+    donateRemainingBalanceOf: "",
+    submitPayoutData: "",
   },
   topup: {
     onlineTopUp: "Online-Aufladung",
-    description: "Du kannst dein Festival-Guthaben hier mit Kreditkarte aufladen.",
+    description: "Du kannst dein Guthaben hier mit Kreditkarte aufladen.",
     amount: "Betrag",
     errorWhileCreatingCheckout: "Fehler beim erstellen der SumUp-Zahlung.",
     errorAmountGreaterZero: "Betrag muss größer als 0 sein.",
@@ -88,67 +87,56 @@ export const translations: NestedPartialAsStrings<Translations> = {
     0: {
       question: "Was ist StuStaPay? (Oder: Hilfe! Der Kassierer will mein Bargeld nicht!)",
       answer:
-        "Dieses Jahr verwenden wir auf dem Festival erstmals ein selbst entwickeltes bargeldloses Bezahlsystem: " +
-        "StuStaPay. An deinem Eintrittsband befindet sich ein Chip, der dein Konto identifiziert, auf das du Guthaben aufladen kannst. Das heißt, dass du an unseren Ständen nicht mehr mit Bargeld hantieren musst, sondern bequem mit deinem Eintrittsbändchen zahlen kannst. Wie das ganze funktioniert erfährst du hier.",
+        "Dieses Jahr verwenden wir auf dem Fest erstmals ein selbst entwickeltes bargeldloses Bezahlsystem: " +
+        "StuStaPay. In deiner Karte befindet sich ein Chip, der dein Konto identifiziert, auf das du Guthaben aufladen kannst. Das heißt, dass du an unseren Ständen nicht mehr mit Bargeld hantieren musst, sondern bequem mit deiner Karte zahlen kannst. Wie das ganze funktioniert erfährst du hier.",
     },
     1: {
-      question: "Wo bekomme ich ein Bändchen?",
+      question: "Wo bekomme ich eine Karte?",
       answer:
-        "Das hast du hoffentlich schon gekauft, als du auf das Festival gekommen bist. Falls du noch kein Bändchen hast, kannst du nachträglich eines am Festzelt, beim KADE oder im Atrium erwerben. Da das Bändchen dein Zugang zu deinem Guthaben ist, solltest du gut darauf aufpassen. Notiere dir am besten jetzt schon die beiden PINs auf der Rückseite. Mit diesen kannst du im Notfall dein Bändchen tauschen lassen.",
+        "Die Karte bekommst du entweder beim Vorverkauf, wobei diese hier auch als Ticket dient, oder am Einlass an der Abendkasse. Beide müssen am Einlass aktiviert werden, bevor sie auf dem Fest verwendet werden können.",
     },
     2: {
       question: "Wo kann ich StuStaPay verwenden?",
       answer:
-        "Du kannst an allen teilnehmenden Verkaufsstellen bezahlen. Dieses Jahr wird an allen Bier-, Wein- und Cocktailausschänken sowie dem Brotladen StuStaPay verwendet. Unsere externen Essens- und Getränkestände sind leider noch nicht mit einbezogen." +
-        "Ausschänke und Verkaufsstellen mit StuStaPay: " +
-        "Festzelt, " +
-        "Weißbierinsel, " +
-        "Weißbierkarussell, " +
-        "Pot-Zelt, " +
-        "Ausschank TribüHne, " +
-        "KADE (Innen- und Außenbereich), " +
-        "Cocktailzelt, " +
-        "Weinzelt, " +
-        "Cuba Lounge und " +
-        "Brotladen",
+        "Du kannst an allen Verkaufsstellen mit StuStaPay bezahlen.",
     },
     3: {
       question: "Wo kann ich sehen wie viel Geld ich habe?",
       answer:
-        "Bei jedem Kauf und jeder Aufladung siehst du dein Guthaben. Auf pay.stustaculum.de kannst du außerdem jederzeit dein aktuelles Guthaben einsehen. Zusätzlich kannst du auch an jeder Kasse dein Guthaben anzeigen lassen.",
+        "Bei jedem Kauf und jeder Aufladung siehst du dein Guthaben. Auf pay.fsei.de kannst du außerdem jederzeit dein aktuelles Guthaben einsehen. Zusätzlich kannst du auch an jeder Kasse dein Guthaben anzeigen lassen.",
     },
     4: {
       question: "Wo kann ich Guthaben aufladen?",
       answer:
-        "An einer unserer Aufladestationen im Atrium, am Festzelt, beim Café Dada oder am KADE kannst du mit Bargeld, Karte oder kontaktloser Bezahlung Guthaben auf dein Bändchen buchen. Das Guthaben steht dir danach sofort für die nächste Maß Bier (und natürlich jedes andere Produkt deiner Wahl) zur Verfügung. Schau am besten auf den [Geländeplan](link), wo sich die nächstgelegene Station befindet.",
+        "An einer unserer Aufladestationen im Audimax Foyer, bei der Garderobe oder am Einlass kannst du mit Bargeld, Karte oder kontaktloser Bezahlung Guthaben auf deine Karte buchen. Zudem kannst du dein Guthaben online unter pay.fsei.de aufladen. Das Guthaben steht dir danach sofort für die nächste Maß Bier (und natürlich jedes andere Produkt deiner Wahl) zur Verfügung.",
     },
     5: {
       question: "Wie funktioniert die Bezahlung?",
       answer:
-        "Nachdem deine Bestellung aufgenommen wurde, wird dein Chip am Eintrittsband eingelesen. Danach kannst du deine Bestellzusammenfassung und dein Guthaben sehen. Nach deinem OK wird die Bestellung gebucht und du bekommst dein gewünschtes Produkt.",
+        "Nachdem deine Bestellung aufgenommen wurde, wird dein Chip in deiner Karten eingelesen. Danach kannst du deine Bestellzusammenfassung und dein Guthaben sehen. Nach deinem OK wird die Bestellung gebucht und du bekommst dein gewünschtes Produkt.",
     },
     6: {
       question: "Wie bekomme/verwende ich Getränkegutscheine?",
       answer:
-        "Gutscheine bekommst du als Belohnung für absolvierte Helferschichten. Diese werden ebenfalls digital über dein Bändchen verwaltet. Nach deiner Schicht übertragt deine Standleitung die Helfermarken auf dein Bändchen. Bei manchen Helferschichten (zum Beispiel beim Abbau) kannst du deine Gutscheine auch schon früher gegen Pfand erhalten. Die Gutscheine werden bei deiner nächsten Bestellung automatisch verrechnet. Wenn du die Gutscheine nicht oder nur teilweise einsetzen möchtest, kannst du den Vorschlag einfach ändern.",
+        "Gutscheine bekommst du als Belohnung für absolvierte Helferschichten. Diese werden ebenfalls digital über deine Karte verwaltet. Nach deiner Schicht übertragt deine Standleitung die Helfermarken auf deine Karte. Bei manchen Helferschichten (zum Beispiel beim Abbau) kannst du deine Gutscheine auch schon früher gegen Pfand erhalten. Die Gutscheine werden bei deiner nächsten Bestellung automatisch verrechnet. Wenn du die Gutscheine nicht oder nur teilweise einsetzen möchtest, kannst du den Vorschlag einfach ändern.",
     },
     7: {
       question: "Wie kann ich prüfen, was ich ausgegeben habe?",
-      answer: "Auf pay.stustaculum.de kannst du deine Belege und dein Guthaben sehen.",
+      answer: "Auf pay.fsei.de kannst du deine Belege und dein Guthaben sehen.",
     },
     8: {
       question: "Wie viel Geld kann ich aufladen?",
-      answer: "Du kannst maximal 150€ auf dein Bändchen laden.",
+      answer: "Du kannst maximal 50€ auf dein Bändchen laden.",
     },
     9: {
       question: "Kann ich auch bar oder mit Karte bezahlen?",
       answer:
-        "Nein, der Aufwand hierfür wäre leider viel zu hoch. An allen teilnehmenden Stationen wird nur noch StuStaPay als Zahlungsmittel akzeptiert. Du kannst dein Bändchen aber mit Bargeld oder Karte aufladen. An Ständen ohne StuStaPay wird weiterhin Bargeld akzeptiert.",
+        "Nein, der Aufwand hierfür wäre leider viel zu hoch. An allen Ständen wird nur noch StuStaPay als Zahlungsmittel akzeptiert. Du kannst deine Karte aber mit Bargeld oder Karte aufladen.",
     },
     10: {
       question: "Was passiert wenn ich am Ende noch Guthaben übrig habe?",
       answer:
-        "Du kannst dir das Guthaben nach Ende des Festivals von uns überweisen lassen, indem du auf pay.stustaculum.de deine Bankdaten hinterlegst. Nach dem 30.09.2023 sind keine Auszahlungen mehr möglich.",
+        "Du kannst dir das Guthaben beim Ausgang an der Garderobe auszahlen lassen. Nach dem Fest sind keine Auszahlungen mehr möglich.",
     },
     11: {
       question: "Warum könnt ihr die Rückzahlung nicht zusichern?",
@@ -156,37 +144,36 @@ export const translations: NestedPartialAsStrings<Translations> = {
         "Das hat rechtliche Gründe. Wenn wir die Rückzahlung garantieren, wären wir quasi eine Bank und müssten entsprechend teure Lizenzen einholen. Keine Angst, du bekommst dein Restguthaben trotzdem zurück.",
     },
     12: {
-      question: "Kann ich mein Guthaben auf dem Festival auszahlen lassen?",
+      question: "Kann ich mein Guthaben auf dem Fest auszahlen lassen?",
       answer:
-        "Wenn die Schlangen nicht zu lang sind, kannst du an einer Aufladestation dein Guthaben als Bargeld auszahlen lassen. Wir empfehlen dir aber, dir das Geld überweisen zu lassen.",
+        "Du kannst dir dein Guthaben nur bei Verlassen des Festes gegen Eintausch deiner Karte auszahlen lassen.",
     },
     13: {
       question: "Hilfe! Mein Chip ist kaputt!",
       answer:
-        "Wende dich ans Infozelt. Dort wird dein Eintrittsband ausgetauscht und das Guthaben wird auf das neue Band übertragen.",
+        "Wende dich an eine der Aufladekassen. Dort kann deine Karte ausgetauscht und das Guthaben auf die neue Karte übertragen werden.",
     },
     14: {
       question: "Hilfe! Ich habe meinen Chip verloren!",
       answer:
-        "Falls du eine E-Mail auf pay.stustaculum.de hinterlegt hast, können wir den verlorenen Chip sperren und dir ein neues Band aushändigen. Andernfalls können wir dein Guthaben leider nicht übertragen.",
+        "Falls du deine ID und PIN aufgeschrieben hast, können wir die verlorene Karte sperren und dir eine neue aushändigen. Andernfalls können wir dein Guthaben leider nicht übertragen.",
     },
     15: {
       question: "Ich habe einen Chip gefunden",
-      answer: "Bring diesen bitte zum Infozelt, dort sammeln wir alle Fundsachen.",
+      answer: "Bring diesen bitte zu einer Aufladekasse. Dort sammeln wir alle Fundsachen.",
     },
     16: {
       question: "Welche Daten werden von mir gespeichert?",
       answer:
-        "Wir speichern zu deinem Bändchen folgende Informationen auf unserem zentralen Server:" +
+        "Wir speichern zu deinem Bändchen folgende Informationen pseudonymisiert auf unserem zentralen Server:" +
         " Dein Kontostand," +
-        " Alle Transaktionen (Aufladungen, EC, Verkauf, Pfandrückzahlung)," +
-        " Deine Belege und" +
-        " Deine Rücküberweisungsdaten. Mehr informationen findest du in unserer Datenschutzerklärung.",
+        " alle Transaktionen (Aufladungen, EC, Verkauf, Pfandrückzahlung) und" +
+        " deine Belege",
     },
     17: {
       question: "Was passiert bei einem Ausfall von StuStaPay?",
       answer:
-        "Sollte StuStaPay aus unvorhergesehenen Gründen ausfallen, werden wir schnellstmöglich auf eine Bezahlung mit Registrierkassen umstellen. Das Restguthaben auf den Bändchen zahlen wir selbstverständlich so schnell wie möglich zurück.",
+        "Sollte StuStaPay aus unvorhergesehenen Gründen ausfallen, werden wir schnellstmöglich auf eine Bezahlung mit Bargeld umstellen. Das Restguthaben auf den Karten zahlen wir selbstverständlich so schnell wie möglich zurück.",
     },
   },
 } as const;
--- web/apps/customer-portal/src/assets/locales/en/translations.ts
+++ web/apps/customer-portal/src/assets/locales/en/translations.ts
@@ -2,8 +2,8 @@ export const translations = {
   StuStaPay: "StuStaPay",
   logout: "Logout",
   login: "Login",
-  userTagUid: "Wristband Tag ID",
-  userTagPin: "Wristband Tag Pin",
+  userTagUid: "Card ID",
+  userTagPin: "Card Pin",
   nav: {
     payout: "Pay Out",
     topup: "Top Up",
@@ -12,13 +12,13 @@ export const translations = {
   },
   loginFailed: "Login failed: {{reason}}.",
   errorLoadingCustomer: "Error loading customer",
-  payoutInfo: "To get your payout after the festival, please <1>enter your bank account details here</1>. The first payout is scheduled approximately 3 weeks after the end of the festival.",
+  payoutInfo: "",
   about: "About",
   contact: "Contact",
-  wristbandTagExample: "Wristband Tag Example",
-  wristbandTagExampleTitle: "Wristband Tag Example with PIN and ID",
+  wristbandTagExample: "Card Example",
+  wristbandTagExampleTitle: "Card Example with PIN and ID",
   wristbandTagExampleDescription:
-    "You can find your wristband tag ID and PIN on the back of your wristband tag. It should look like the example given below:",
+    "You can find your card ID and PIN on the back of your card. It should look like the example given below:",
   termsAndConditionsHeader:
     "Our Terms and Conditions are only available in German. You can access our privacy policy on <1>here</1>.",
   privacyPolicyHeader:
@@ -28,7 +28,7 @@ export const translations = {
     de: "Deutsch",
   },
   balance: "Balance",
-  tagUid: "Wristband Tag ID",
+  tagUid: "Card ID",
   vouchers: "Vouchers",
   order: {
     loadingError: "Error loading orders",
@@ -47,26 +47,25 @@ export const translations = {
     },
   },
   payout: {
-    iban: "IBAN",
-    bankAccountHolder: "Account Holder",
-    email: "E-Mail",
-    info: "Please enter your bank account information so we can transfer your leftover balance. You can also donate parts or your whole remaining balance to support our volunteer work. Kulturleben in der Studentenstadt e. V. is a student-run non-profit organization which annually holds the StuStaCulum festival. The first payout is planned approximately 3 weeks after the end of the festival.",
-    ibanNotValid: "IBAN is not valid",
-    countryCodeNotSupported: "Provided IBAN country code is not supported",
-    mustAcceptPrivacyPolicy: "You must accept the privacy policy",
-    privacyPolicyCheck: "I have read and agree to StuStaCulum's <1>privacy policy</1>.",
-    errorFetchingData: "Error fetching data.",
-    updatedBankData: "Successfully updated bank data. The first payout is expected to happen approximately 3 weeks after the end of the festival.",
-    errorWhileUpdatingBankData: "Error occurred while updating bank data.",
-    donationMustBePositive: "Donation must be positive",
-    donationExceedsBalance: "Donation cannot exceed your balance",
-    donationTitle: "Donation",
-    payoutTitle: "Payout",
-    donationAmount: "Donate amount",
-    donationDescription:
-      "If you appreciated our hard work to make this festival come true, we also welcome donations in order to support our future work",
-    donateRemainingBalanceOf: "Donate remaining balance of ",
-    submitPayoutData: "Save bank data",
+    iban: "",
+    bankAccountHolder: "",
+    email: "",
+    info: "",
+    ibanNotValid: "",
+    countryCodeNotSupported: "",
+    mustAcceptPrivacyPolicy: "",
+    privacyPolicyCheck: "",
+    errorFetchingData: "",
+    updatedBankData: "",
+    errorWhileUpdatingBankData: "",
+    donationMustBePositive: "",
+    donationExceedsBalance: "",
+    donationTitle: "",
+    payoutTitle: "",
+    donationAmount: "",
+    donationDescription: "",
+    donateRemainingBalanceOf: "",
+    submitPayoutData: "",
   },
   topup: {
     amount: "Amount",
@@ -91,66 +90,55 @@ export const translations = {
     0: {
       question: "What is StuStaPay? (Or: Help! The cashier doesn't want my cash!)",
       answer:
-        "This year, we are using a self-developed cashless payment system called StuStaPay for the festival. Your entry wristband contains a chip that identifies your account, which you can top up with credit. This means that you no longer need to handle cash at our stands, but can conveniently pay with your entry wristband. Find out how it all works here.",
+        "This year, we are using a self-developed cashless payment system called StuStaPay. Your card/ticket contains a chip that identifies your account, which you can top up with credit. This means that you no longer need to handle cash at our stalls, but can conveniently pay with your card. Find out how it all works here.",
     },
     1: {
-      question: "Where can I get a wristband?",
+      question: "Where can I get a card?",
       answer:
-        "Hopefully, you have already purchased one when you arrived at the festival. If you don't have a wristband yet, you can buy one afterwards at the festival tent, at KADE, or in the atrium. Since the wristband is your access to your credit, you should take good care of it. It's best to note down the two PINs on the back right now. With these, you can have your wristband replaced in case of emergency.",
+        "You can purchase a card at our presale, in which case it will also function as a ticket, or at the entrance to the party. Note that your card must be activated at the entrance before it can be used during the party.",
     },
     2: {
       question: "Where can I use StuStaPay?",
       answer:
-        "You can pay at all participating sales points. This year, StuStaPay will be used at all beer, wine, and cocktail bars, as well as the bread shop. Unfortunately, our external food and beverage stands are not included yet." +
-        "Bars and sales points accepting StuStaPay: " +
-        "Festzelt, " +
-        "Weißbierinsel, " +
-        "Weißbierkarussell, " +
-        "Pot-Zelt, " +
-        "Ausschank TribüHne, " +
-        "KADE (indoor and outdoor areas), " +
-        "Cocktailzelt, " +
-        "Weinzelt, " +
-        "Cuba Lounge and " +
-        "Brotladen",
+        "You can use StuStaPay at all of our stalls.",
     },
     3: {
       question: "Where can I see how much money I have?",
       answer:
-        "You can see your balance with each purchase and top-up. Additionally, you can view your current balance at any time on pay.stustaculum.de. You can also have your balance displayed at any cash register.",
+        "You can see your balance with each purchase and top-up. Additionally, you can view your current balance at any time on pay.fsei.de. You can also have your balance displayed at any cash register.",
     },
     4: {
       question: "Where can I top up my credit?",
       answer:
-        "You can top up your wristband with cash, card, or contactless payment at one of our top-up stations in the atrium, at the festival tent, Café Dada, or at KADE. The credit will be available for your next beer (and any other product of your choice) immediately after topping up. Check the [site map](link) to find the nearest station.",
+        "You can top up your card with cash, card, or contactless payment at one of our top-up stations in the Audimax, at the cloakroom or at the entrance. You may also top up your card online at pay.fsei.de. The credit will be available for your next beer (and any other product of your choice) immediately after topping up.",
     },
     5: {
       question: "How does the payment process work?",
       answer:
-        "After your order is taken, your chip on the entry wristband is read. Then, you can see your order summary and your credit. After your confirmation, the order is processed, and you will receive your desired product.",
+        "After your order is taken, your card is read. Then, you can see your order summary and your credit. After your confirmation, the order is processed, and you will receive your desired product.",
     },
     6: {
       question: "How can I get/use drink vouchers?",
       answer:
-        "You receive vouchers as a reward for completed volunteer shifts. These vouchers are also managed digitally on your wristband. After your shift, the stand management transfers the volunteer tokens to your wristband. For some volunteer shifts (e.g., during dismantling), you can receive your vouchers in advance in exchange for a deposit. The vouchers will be automatically deducted from your next order. If you don't want to use the vouchers or only use them partially, you can simply change the suggestion.",
+        "You can receive vouchers as a reward for completed volunteer shifts. These vouchers are also managed digitally on your card. After your shift, the stall management will transfer the volunteer tokens to your card. For some volunteer shifts (e.g., during dismantling), you can receive your vouchers in advance in exchange for a deposit. The vouchers will be automatically deducted from your next order. If you don't want to use the vouchers or only use them partially, you can simply change the suggestion.",
     },
     7: {
       question: "How can I check what I have spent?",
-      answer: "You can see your receipts and your balance on pay.stustaculum.de.",
+      answer: "You can see your receipts and your balance on pay.fsei.de.",
     },
     8: {
-      question: "How much money can I load onto my wristband?",
-      answer: "You can load a maximum of 150€ onto your wristband.",
+      question: "How much money can I load onto my card?",
+      answer: "You can load a maximum of 50€ onto your card.",
     },
     9: {
       question: "Can I also pay with cash or card?",
       answer:
-        "No, the effort for this would be too high. Only StuStaPay is accepted as a payment method at all participating stations. However, you can load your wristband with cash or card. Cash is still accepted at stands without StuStaPay.",
+        "No, the effort for this would be too high. Only StuStaPay is accepted as a payment method at all stalls. However, you can load your card with cash or card.",
     },
     10: {
       question: "What happens if I have remaining credit at the end?",
       answer:
-        "You can have the remaining credit transferred to your bank account after the festival by providing your bank details on pay.stustaculum.de. After September 30, 2023, no further withdrawals will be possible.",
+        "You can receive the remaining credit at the cloakroom in exchange for your card. After the party no further withdrawals will be possible.",
     },
     11: {
       question: "Why can't you guarantee the refund?",
@@ -160,21 +148,21 @@ export const translations = {
     12: {
       question: "Can I cash out my credit at the festival?",
       answer:
-        "If the lines are not too long, you can have your credit paid out in cash at a top-up station. However, we recommend having the money transferred to your account.",
+        "You may only withdraw credit in exchange for your card when leaving the party.",
     },
     13: {
       question: "Help! My chip is damaged!",
       answer:
-        "Please contact the information tent. They will replace your entry wristband and transfer the credit to the new band.",
+        "Please contact one of our top-up stations. They will replace your card and transfer the credit.",
     },
     14: {
       question: "Help! I lost my chip!",
       answer:
-        "If you have provided an email on pay.stustaculum.de, we can block the lost chip and give you a new wristband. Otherwise, we cannot transfer your credit.",
+        "If you know your card ID and PIN we can block the old card and transfer the credit to a new card. Otherwise, we cannot transfer your credit.",
     },
     15: {
       question: "I found a chip",
-      answer: "Please bring it to the information tent. They collect all lost and found items there.",
+      answer: "Please bring it to one of our top-up stations. They collect all lost and found items there.",
     },
     16: {
       question: "What data is stored about me?",
@@ -183,7 +171,6 @@ export const translations = {
         " Your account balance," +
         " all transactions (top-ups, card payments, sales, deposit refunds)," +
         " your receipts, and" +
-        " your refund transfer details. " +
         "Have a look at our privacy policy for more information.",
     },
     17: {
